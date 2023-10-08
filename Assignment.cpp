af4e05d6ecb7fc141f115f64fa3ed8042f8318c3
GAURAV RAJ SINGH <122299301+GRASiN1@users.noreply.github.com>
2023-10-08 16:48:13 +0530

    Update Assignment.cpp

---
+++ Assignment.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;          // variable name x of type int ---> It can store a single integer  which means x can either store 1 or 2 or any single integer
+
+    // Arrays can be used to store multiple values in a single variable name
+
+    int a[5]; // This means it is an array of name a which can store multiple integers , It can store 6 integers
+                // Note: The indexing of arrays starts from 0 and not 1
+        
+        // elements starting form 0-5 
+
+        // The first element will have an index 0 --> a[0]
+        // The second element will have an index 1 --> a[1]
+
+        // And goes till a[5]
+
+        
+
+}
